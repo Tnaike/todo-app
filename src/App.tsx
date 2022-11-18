b66a4025e889712aa55a99bf5b18f9b9cf3ea888
Tnaike <timmynaike@gmail.com>
2022-11-18 02:37:01 +0100

    created input field implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import BoardBackground from './components/BoardBackground';
+import InputField from './components/InputField';
+import { Todo } from './model';
 
-function App() {
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>('');
+  const [listTodo, setListTodo] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setListTodo([...listTodo, { id: Date.now(), todo: todo, isDone: false }]);
+      setTodo('');
+    }
+  };
+
+  // console.log(todo);
+  console.log('***LIST TODO=>**', listTodo);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        ToDo<span className='altText'>Plus</span>
+        <sup>&trade;</sup>
       </header>
+      <BoardBackground>
+        <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      </BoardBackground>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,14 +1,75 @@
 import React from 'react';
+import styled from 'styled-components';
+import { ReactComponent as AddIcon } from '../assets/add-icon.svg';
+import { colors } from '../styles/index';
 
+const StyledForm = styled.form`
+  position: relative;
+  display: flex;
+  width: 100%;
+  padding: 20px 15px;
+  align-items: center;
+`;
 
-const InputField = () => {
+const Input = styled.input`
+  position: relative;
+  width: 100%;
+  padding: 20px 65px 20px 25px;
+  font-size: 1.4rem;
+  color: ${colors.boldDark};
+  border: none;
+  border-radius: 50px;
+  outline: none;
+  overflow: hidden;
+  box-shadow: inset 0px 0px 5px ${colors.boldDark};
+  &:focus {
+    box-shadow: 0 0px 10px 1000px ${colors.boldDark};
+    outline: none;
+  }
+`;
+
+const SubmitButton = styled.button`
+  position: absolute;
+  width: 50px;
+  height: 50px;
+  right: 25px;
+  background: ${colors.blue};
+  border-radius: 50px;
+  border: none;
+  transition: 0.2s all;
+  -webkit-box-shadow: 0 0 8px ${colors.darkBlue};
+  box-shadow: 0 0 8px ${colors.darkBlue};
+  &:not([disabled]):hover {
+    cursor: pointer;
+    background: ${colors.red};
+  }
+  &:not([disabled]):active {
+    transform: scale(0.9);
+    box-shadow: 0 0 3px ${colors.darkBlue};
+  }
+`;
+
+interface TodoProps {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<TodoProps> = ({ todo, setTodo, handleAdd }) => {
   return (
-    <div>
-      <form className='inputForm'>
-        <input type='text' placeholder='Enter a task' className='input__field'/>
-        <button type='submit' className='input__submit'>Add</button>
-      </form>
-    </div>
+    <>
+      <StyledForm onSubmit={handleAdd}>
+        <Input
+          type='text'
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+          placeholder='Enter a task'
+        />
+        <SubmitButton type='submit' disabled={!todo}>
+          <AddIcon />
+        </SubmitButton>
+      </StyledForm>
+    </>
   );
 };
 
